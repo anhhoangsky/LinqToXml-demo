3437d5a6f4158b613128bef70f04644ab7b77690
anhsky <an38570@gmail.com>
2020-03-15 12:24:08 +0700

    Add project files.

---
+++ LinqToXml/CreateXmlDOM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinqToXml
+{
+    class CreateXmlDOM
+    {
+        static public void Create()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            XDocument xmlStudents = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+
+                new XComment("List Of Student"),
+
+                new XElement("Students",
+                 new XElement("Student",
+                            new XAttribute("id", 1005),
+                            new XElement("Name", "Nguyễn Hoàng Anh"),
+                            new XElement("Gender", "Nam"),
+                            new XElement("Class", "K41E")
+                        ),
+                    new XElement("Student",
+                            new XAttribute("id", 1006),
+                            new XElement("Name", "Huỳnh Ngọc Bảo Phúc"),
+                            new XElement("Gender", "Nam"),
+                            new XElement("Class", "K41E")
+                        ),
+                    new XElement("Student",
+                            new XAttribute("id", 1007),
+                            new XElement("Name", "Võ Trần Nhật Bình"),
+                            new XElement("Gender", "Nam"),
+                            new XElement("Class", "K41E")
+                        ),
+                    new XElement("Student",
+                            new XAttribute("id", 1008),
+                            new XElement("Name", "Nguyễn Văn Diễn"),
+                            new XElement("Gender", "Nam"),
+                            new XElement("Class", "K41E")
+                        ),
+                    new XElement("Student",
+                            new XAttribute("id", 1009),
+                            new XElement("Name", "Nguyễn Minh Tâm"),
+                            new XElement("Gender", "Nam"),
+                            new XElement("Class", "K41E")
+                        ))
+            );
+            //xmlStudents.Save(Console.Out);
+            Console.WriteLine("Done!");
+            xmlStudents.Save(@"D:\web\project_team\linqtoxml\linqtoxml\Data.xml");
+        }
+    }
+}
---
+++ LinqToXml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinqToXml
+{
+    class Program
+    {
+        private static void Main()
+        {
+            //CreateXmlDOM.Create();
+
+        }
+    }
+}
