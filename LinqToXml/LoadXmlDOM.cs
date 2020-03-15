30991d81ca9d026387d4dcacc2330306e816bb70
anhsky <an38570@gmail.com>
2020-03-15 12:52:31 +0700

    add feture load xml file

---
+++ LinqToXml/LoadXmlDOM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace LinqToXml
+{
+    class LoadXmlDOM
+    {
+        public static XmlDocument Load(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            Console.WriteLine(doc.DocumentElement.OuterXml);
+            return doc;
+        }
+    }
+}
